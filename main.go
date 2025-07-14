e62efe6212527ddd184db8a7756ef425221a829f
Souvik-Cyclic <souvik.cyc@gmail.com>
2025-07-15 02:43:06 +0530

    Add message to indicate this is a Jenkins test project in main function

--- main.go
+++ main.go
@@ -5,4 +5,5 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World!")
 	fmt.Println("This is a Jenkins test project.")
+	fmt.Println("Adding a message to indicate this is a Jenkins test project.")
 }
