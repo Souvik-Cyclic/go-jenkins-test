84ef242164f1bbe46687b4946f828cccdbbaf282
Souvik-Cyclic <souvik.cyc@gmail.com>
2025-07-15 02:46:26 +0530

    Remove redundant message indicating Jenkins test project in main function

--- main.go
+++ main.go
@@ -5,5 +5,4 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World!")
 	fmt.Println("This is a Jenkins test project.")
-	fmt.Println("Adding a message to indicate this is a Jenkins test project.")
 }
